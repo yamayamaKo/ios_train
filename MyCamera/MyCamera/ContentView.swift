1f9acf2c5f2176388d791f952c295795f50171be
yamayamaKo <yamachanwow@gmail.com>
2023-05-20 20:10:00 +0900

    2-2終了

--- MyCamera/MyCamera/ContentView.swift
+++ MyCamera/MyCamera/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var captureImage: UIImage? = nil
+    @State var isShowSheet = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            if let captureImage {
+                Image(uiImage: captureImage)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            Spacer()
+            
+            Button {
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    print("カメラは利用できます")
+                    isShowSheet.toggle()
+                } else {
+                    print("カメラは利用できません")
+                }
+            } label: {
+                Text("カメラを起動する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+            }
+            .padding()
+            .sheet(isPresented: $isShowSheet) {
+                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+            }
+            
+            if let captureImage,
+               let shareImage = Image(uiImage: captureImage) {
+                ShareLink(item: shareImage, preview: SharePreview("Photo", image: shareImage)) {
+                    Text("SNSに投稿する")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .padding()
+                }
+            }
+
         }
-        .padding()
     }
 }
 
---
+++ MyCamera/MyCamera/ImagePickerView.swift
@@ -0,0 +1,48 @@
+//
+//  ImagePickerView.swift
+//  MyCamera
+//
+//  Created by 山田紘暉 on 2023/05/20.
+//
+
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    @Binding var isShowSheet: Bool
+    @Binding var captureImage: UIImage?
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePickerView
+        
+        init(_ parent: ImagePickerView) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.captureImage = originalImage
+            }
+            
+            parent.isShowSheet.toggle()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.isShowSheet.toggle()
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let myImagePickerController = UIImagePickerController()
+        myImagePickerController.sourceType = .camera
+        myImagePickerController.delegate = context.coordinator
+        return myImagePickerController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
