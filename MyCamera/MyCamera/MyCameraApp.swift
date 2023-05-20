69c3999154d76aaf112c52987e5f1bde71617435
yamayamaKo <yamachanwow@gmail.com>
2023-05-20 18:57:15 +0900

    first commit

---
+++ MyCamera/MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 山田紘暉 on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
